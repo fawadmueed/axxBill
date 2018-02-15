96449781aca541f6c64442b96d6387816a84c528
Alexey Kryukov <akryukov@semiosis.com>
2018-02-15 10:00:08 -0500

    Plan traitement implementation

--- javascript/axxium/planTraitement.js
+++ javascript/axxium/planTraitement.js
@@ -25,9 +25,26 @@ function PlnTrPopulateDetails()
 function PlnTrSavePlan()
 {
     getAllTrData_planTrait();
+    var cdaNetRequest = '';
+    var plnTrInfo = arrGrilleDeFacturation_planTrait;
+    var randomNum = CdaCommCreateRandomNumber(1, 999);
+    var inputXMl = {
+        "request": cdaNetRequest, //request to send
+        "inputs": plnTrInfo // JSON data
+    };
+
+    $.post("allScriptsv1.py", { tx: "SendPlanTraitement", clinicId: globClinicId, patientId: globPatientId, nodossier: globNoDossier, lun: randomNum, json: JSON.stringify(inputXMl), sendReq: false },
+        function (result) {
+            var xml = result.message;
+            $("#message").append(xml);
+        });
 }
 
 function PlnTrSendPlan()
 {
     getAllTrData_planTrait();
+}
+
+function PlnTrCreateNew() {
+    $('.PlanTrait').modal('hide');
 }

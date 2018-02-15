50bc7c66a4794634c44469c8d0f615a55ad2b94f
Alexey Kryukov <akryukov@semiosis.com>
2018-02-15 09:41:35 -0500

    plan traitement implement

---
+++ javascript/axxium/planTraitement.js
@@ -0,0 +1,33 @@
+
+//arrGrilleDeFacturation_planTrait
+function PlnTrStart()
+{
+    PlnTrGetList();
+}
+
+function PlnTrGetList()
+{
+    var isListEmpty = false;
+    if (!isListEmpty)
+        modPlanTraitTable();
+}
+
+function PlnTrListPopulateTable()
+{
+    //display list popup
+}
+
+function PlnTrPopulateDetails()
+{
+
+}
+
+function PlnTrSavePlan()
+{
+    getAllTrData_planTrait();
+}
+
+function PlnTrSendPlan()
+{
+    getAllTrData_planTrait();
+}

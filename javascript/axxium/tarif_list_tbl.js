d51acd3d393707f069639c94435ae8b9edf19674
Fawad Mueed <fawad.mueed@nuvoola.com>
2018-03-07 11:24:38 -0500

    'UpdateTablueUi'

--- javascript/axxium/tarif_list_tbl.js
+++ javascript/axxium/tarif_list_tbl.js
@@ -19,12 +19,12 @@ $(document).ready(function(){
  //    $('#myTable').DataTable().search(this.value.trim(), true, false).draw();
  // });   
 
-	// $('.searchTarif').click(function(){
-	// 	var srch='\\b'+$(this).val();
-	// 	console.log(srch);
-	// 	tarifTbl_datTbl.column(0).search(srch,true,false).draw();
+	$('.searchTarif').click(function(){
+		var srch='\\b'+$(this).val();
+		console.log(srch);
+		tarifTbl_datTbl.column(0).search(srch,true,false).draw();
 		
-	// })
+	})
 })
 
 function fact_tarif_list(x)
@@ -33,14 +33,16 @@ function fact_tarif_list(x)
 		var this_row_id=$(x).parent('tr').attr('id');
 		globVarSurfValidation_this_row_id=this_row_id;
 
-		// $.fn.dataTable.ext.errMode = 'none';
+		$.fn.dataTable.ext.errMode = 'none';
 		$.fn.dataTable.render.number( '.', ',', 2, '' );
 		// Getting data for the Modal, Mapping it from JSON to ARRAY
 		var arr = $.map(dataJson_Code, function(val,key) { return {code:key,value:val} });
 		//Popup Modal
 		popTarifTbl(arr);
 		
-		$('.fact_tarif_list.modal').modal('show');
+		$('.fact_tarif_list.modal').modal({
+			observeChanges: true
+		}).modal('show');
 
 	}
 
